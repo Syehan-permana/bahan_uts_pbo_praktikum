52bfd41afb19e8010b6ab5cd88546297b8f07f34
Syehan-permana <sehan05133@gmail.com>
2025-12-10 09:17:56 +0700

    Add files via upload

---
+++ p10/bertahanhidup.java
@@ -0,0 +1,16 @@
+//kumpulan metode
+package p10.abstrak;
+public interface bertahanhidup {
+    void makan();
+    void minum();
+    void tidur();
+}
+
+interface Memangsa{
+    void memakan();
+    void menguliti();
+    void menusuk();
+    
+    
+    
+}
